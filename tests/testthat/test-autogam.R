96634c3aefd2456f8c5dd7a0fbb97f75941cbce7
Chitu Okoli <Chitu.Okoli@skema.edu>
2024-09-27 13:22:58 +0200

    Swapped order of expect_equal

--- tests/testthat/test-autogam.R
+++ tests/testthat/test-autogam.R
@@ -1,9 +1,9 @@
 test_that("autogam works on mtcars", {
   expect_equal(
-    11.253,
     autogam(mtcars, 'mpg') |>
       coef() |>
       sum() |>
-      round(3)
+      round(3),
+    11.253
   )
 })
--- tests/testthat/test-smooth_formula_string.R
+++ tests/testthat/test-smooth_formula_string.R
@@ -1,6 +1,7 @@
 test_that("mtcars smooth string works", {
   expect_equal(
-    "mpg ~ cyl + s(disp) + s(hp) + s(drat) + s(wt) + s(qsec) + vs + am + gear + s(carb,k=3)",
-    smooth_formula_string(mtcars, 'mpg')
+    smooth_formula_string(mtcars, 'mpg'),
+    "mpg ~ cyl + s(disp) + s(hp) + s(drat) + s(wt) + s(qsec) + vs + am + gear + s(carb,k=3)"
+
   )
 })
